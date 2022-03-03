41f2463f5420eb4dcd56b37e65905d58cdebd813
Angel Valentin <angelgvalentin7@gmail.com>
2022-03-03 14:34:07 -0600

    created cards and detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 
 const Detail = (props) => {
-  return (
-    <section className="log-details">
-      <h2>Travel Experience Details</h2>
-      <ul>
-        <li>{props.allLogs.name}</li>
-        <li>{props.allLogs.location}</li>
-        <li>{props.allLogs.description}</li>
-        <li>${props.allLogs.cost}</li>
-        <li><img src={props.allLogs.image} /></li>
-        <li>{props.allLogs.recommendation}</li>
-      </ul>
-    </section>
-  )
+    return (
+        <section className="log-details">
+            <h2>Travel Experience Details</h2>
+            <ul>
+                <li>{props.selectedLog.name}</li>
+                <li>{props.selectedLog.location}</li>
+                <li>{props.selectedLog.description}</li>
+                <li>${props.selectedLog.cost}</li>
+                <li>
+                    <img src={props.selectedLog.image} />
+                </li>
+                <li>{props.selectedLog.recommendation}</li>
+            </ul>
+        </section>
+    );
 };
 
 export default Detail;
--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,9 +1,33 @@
-import { appendChild } from "parse5/lib/tree-adapters/default";
 import { useState } from "react";
 import "./Logs.css";
 
 const Logs = (props) => {
-    return <img src={props.allLogs[3].image}></img>;
+    return (
+        <section>
+            <h1>test</h1>
+            <div className="cardContainer">
+                {props.allLogs.map((log) => {
+                    return (
+                        <a
+                            key={log._id}
+                            className="mouseOver"
+                            onClick={() => {
+                                props.handleLogSelect(log);
+                            }}
+                        >
+                            <section className="cards">
+                                <img src={log.image} alt=""></img>
+
+                                <div>
+                                    <p>Click for more info</p>
+                                </div>
+                            </section>
+                        </a>
+                    );
+                })}
+            </div>
+        </section>
+    );
 };
 
 export default Logs;
