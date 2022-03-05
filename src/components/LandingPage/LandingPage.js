17c9dd494af05f395232733a8afb7aa822c85a2f
Angel Valentin <angelgvalentin7@gmail.com>
2022-03-05 11:39:30 -0600

    added landding page comp

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./LandingPage.css";
+
+const LandingPage = (props) => {
+    return (
+        <section>
+            <div className="Banner">
+                <h1>Landing Page</h1>
+            </div>
+        </section>
+    );
+};
+
+export default LandingPage;
