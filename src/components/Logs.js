f8f420b0be51d46e5a970c4306736a9d4d0e9051
Kevin Dutile <4323281+kdutile@users.noreply.github.com>
2022-03-06 22:08:56 -0500

    log toggle and it's fiiiiiiiire

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,14 +1,37 @@
 import "./Logs.css";
+import { useState } from "react";
+
 
 const Logs = (props) => {
 
-    const userLogs = props.allLogs.filter(log => props.currentUser.username === log.user)
+    const [showAll, setShowAll] = useState(false);
+    const [displayLogs, setDisplayLogs] = useState(props.allLogs.filter(log => props.currentUser.username === log.user));
+
+    const shownLogs = [];
+
+    const showAllToggler = () => {
+      if (showAll) {
+        setShowAll(false);
+        setDisplayLogs(props.allLogs.filter(log => props.currentUser.username === log.user));
+      } else {
+        setShowAll(true);
+        setDisplayLogs(props.allLogs);
+      }
+    }
 
     return (
         <section className="logsBackground">
-            {/* <h1>test</h1> */}
+            { showAll ? <div className="buttons has-addons">
+                          <button className="button" onClick={showAllToggler}>Only Mine</button>
+                          <button className="button is-info is-selected">Show All</button>
+                        </div>
+                      : <div className="buttons has-addons">
+                          <button className="button is-info is-selected">Only Mine</button>
+                          <button className="button" onClick={showAllToggler}>Show All</button>
+                        </div>
+            }
             <div className="cardContainer">
-                {userLogs.map((log) => {
+                {displayLogs.map((log) => {
                     return (
                         <div
                             key={log._id}
